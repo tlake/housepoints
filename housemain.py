fec645d577177746d5aa39e93f445eb119ef4a60
tanner <tanner.lake@gmail.com>
2014-02-12 21:29:13 -0800

    Code-reviewed by Nate; notes follow.
    CHANGES AND LEARNING:
    - Newline definitions were unnecessary and bad ideas
    - My file handling was sloppy and unnecessary --> IOError
    - Was using ArgParser a bit incorrectly
    	- no need for separate -a and -s
    	- is better suited to also be handling the location of
    	  my files, allowing for user-defined paths
    - Learned that it's a terrible idea to prepend to files; if a
      file is fragmented, prepending requires the loading of the
      first chunk, pushing everything out, then finding the next
      chunk to insert the displaced line from the prepending,
      which in turn will load (and push out) the next chunk, and so
      on until it gets to the end of the file. Appending only
      requires fiddling with the final piece, so we fixed that.
    - In that vein, it was then silly to keep the function which
      returned the first line of the file.
    - It's silly to name a variable 'xyz_str' when its name has no
      bearing on its type.
    - Python can Pickle objects to store them as non-human-readable
      file, then unpickle them to restore the object back to pre-
      pickled status. Pretty cool.
    - Holy crap, such a cool thing with multiplying by 1 or -1.
    
    OTHER THINGS TO DO:
    - Possibly remove the subtraction arg entirely
    - Make totals message display after adding/subtracting
    - Look into possibly using a database to store things like
      diff/timestamp/reason (SQLite)

--- housemain.py
+++ housemain.py
@@ -3,69 +3,43 @@ import shutil
 import os
 import sys
 
-# python version check, newline character def
-if sys.version_info.major == 3:
-	newline = '/n'
-else:
-	newline = '\n'
-
-# some defs
-new_total_str = 'New House Points Total: '
-current_total_str = 'Current House Points Total: '
-homedir = os.path.expanduser('~')
-points_dir = homedir + '/local/share/'
-points_file = 'pointstracker'
-points_path = points_dir + points_file
-
-def file_checker(dirname, filename):
-	if not os.path.isdir(dirname):
-		print  'Directory not found; creating ' + points_path
-		os.makedirs(dirname)
-		with open(dirname + filename, 'w') as newfile:
-			newfile.write('0')
-		return
-	elif not os.path.isfile(dirname + filename):
-		print 'File \'pointstracker\' not found; creating ' + points_path
-		with open(dirname + filename, 'w') as newfile:
-			newfile.write('0')
-		return
-	return
-
-def file_reader(path_to_file):
-	with open(path_to_file, 'r') as oldfile:
-		return oldfile.readline()
-
-def file_updater(path_to_file, string_to_add):
-	with open('temp', 'w') as tempfile:
-		with open(path_to_file, 'r') as oldfile:
-			tempfile.write(string_to_add + newline + oldfile.read())
-	shutil.move('temp', path_to_file)
-
+default_store = os.path.join(os.path.expanduser('~'), 'local', 'share', 'pointstracker')
 # parser setup
 parser = argparse.ArgumentParser(description='display current house points')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-a', '--add', action='store', type=int)
-group.add_argument('-s', '--subtract', action='store', type=int)
+parser.add_argument('--storefile',
+	nargs='?',
+	default=default_store,
+	metavar='FILE',
+	help='default ~/local/share/pointstracker')
+parser.add_argument('-s',
+	'--subtract',
+	dest='subtract',
+	action='store_true',
+	help='if given, will subtract amounts')
+parser.add_argument('amounts',
+	metavar='N',
+	type=int,
+	nargs='*',
+	help='amounts to add/subtract')
 
 args = parser.parse_args()
+if not os.path.exists(os.path.dirname(args.storefile)):
+	os.makedirs(os.path.dirname(args.storefile))
 
+# some defs
+new_total = 'New House Points Total: '
+current_total = 'Current House Points Total: '
 
-# if block: how to handle arguments
-## adding points
-if args.add:
-	file_checker(points_dir, points_file)
-	points = str(int(file_reader(points_path)) + args.add)
-	print new_total_str + points
-	file_updater(points_path, points)
-
-## subtracting points
-elif args.subtract:
-	file_checker(points_dir, points_file)
-	points = str(int(file_reader(points_path)) - args.subtract)
-	print new_total_str + points
-	file_updater(points_path, points)
-
-## displaying current points (default behavior)
-else:
-	file_checker(points_dir, points_file)
-	print current_total_str + file_reader(points_path)
+try:
+	if not args.amounts:
+		with open(args.storefile, 'r') as f:
+			print sum(int(line) for line in f)
+	else:
+		# begin my favorite line
+		numbers = ((-1 if args.subtract else 1) * n for n in args.amounts)
+		# end my favorite line
+		with open(args.storefile, 'a') as f:
+			f.writelines(str(n) for n in numbers)
+			f.write('\n')
+except IOError as e:
+	print e.strerror
